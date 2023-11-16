f6f747074b70ad507a42ad398dc3e2e4ce46ab2b
Angel <ata@loriot.io>
2023-11-16 16:02:36 +0100

    Characters list * Implemented Characters list

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -1,16 +1,30 @@
+import Image from 'next/image';
+
 import { PUBLIC_API_KEY, HASH, PRIVATE_API_KEY } from '@/secrets/keys';
 import { USERS } from './data';
+import classes from './page.module.css';
 const crypto = require('crypto')
 
 export default async function UsersPage() {
   const ts = Date.now();
-  let hash = crypto.createHash('md5').update(`${ts}${PRIVATE_API_KEY}${PUBLIC_API_KEY}`).digest("hex");
+  let hash = crypto.createHash('md5').update(`${ts}${PRIVATE_API_KEY}${PUBLIC_API_KEY}`).digest('hex');
   // let data = await fetch(`https://gateway.marvel.com:443/v1/public/characters?ts=${ts}&apikey=${PUBLIC_API_KEY}&hash=${hash}&limit=10`).then(response => response.json());
 
   return (
     <>
       <h1>Users Page</h1>
-      {USERS.map((character: any) => <p key={character.id}>{character.name}</p>)}
+      <div className={classes['characters-list']}>
+        {USERS.map((character: any) => {
+          // return (<p key={character.id}>{character.name}</p>)
+          return (
+            <div className={classes.character} key={character.id}>
+              <Image alt="character-image" src={character.thumbnail.path+'.'+character.thumbnail.extension} width={50} height={50} />
+              <h2>{character.name}</h2>
+              <p>{character.modified}</p>
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
